a5639ce58d8788eacf626949a6fb00e4b2128a9a
abannon6601 <a.bannon6601@gmail.com>
2018-04-29 16:51:37 -0400

    Gennome generation functional

--- main.cpp
+++ main.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <math.h>
+#include <random>
 #include <functional>
+#include <thread>
+#include <stdlib.h>
+#include <math.h>
 
-float func_sin_1(float sign, float k, float x);
-float func_exp_1(float sign, float k,  float x);
 
-float RandomFloat(float a, float b);
+
+// ----------------------global constant delcrations--------------------------------------------------------------------
+
+const int X = 3;              // X is the length of the data (how many variables we consider)
+#define MAX_THREADS 4;      // max number of threads the program will use (could switch to system value later)
+const int GENOME_LENGTH = 4;  // length of each genome
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+float   func_sin_1(float sign, float k, float x);
+float   func_exp_1(float sign, float k,  float x);
+
+float   RandomFloat(float a, float b);
+int     RandomInt(int a, int b);
+
+void    set_func_arry();
 
 std::vector <std::function<float(float,float,float)>> functions; // global list of function pointers
-
-
 
 class gene
 {
@@ -20,9 +35,10 @@ class gene
     float k; //scalling factor
     std::function<float(float,float,float)> gene_func;  // pointer to funciton implemented
 
-    public:
-
     int dataIndex;  // index of the data vector this gene operates on
+    // ^ this is only held as a record. When the gene is to be passed data this should be used to determine what data
+
+    public:
 
 
     void set_sign(float in_sign)
@@ -31,11 +47,20 @@ class gene
     }
     void set_k(float in_k)
     {
-        sign = in_k;
+        k = in_k;
     }
     void set_gene_func(std::function<float(float,float,float)> new_func)
     {
         gene_func = new_func;
+    }
+    void set_dataIndex(int newIndex)
+    {
+        dataIndex = newIndex;
+    }
+
+    int get_dataIndex()
+    {
+        return dataIndex;
     }
 
     float produce(float x)  // return the result of the gene
@@ -48,30 +73,87 @@ class gene
 
 class genome
 {
+
 public:
 
-     std::vector <gene> genes;  // hold all the genes
+    std::vector <gene*> genes;  // hold all the genes
 
-    gene newGene()  // generated a new random gene
+    genome()
     {
-        gene proc_gene;
+        // fill up the genome with random genes
+        for(int i = 0; i < GENOME_LENGTH; i++)
+        {
+            genes.push_back(this->newGene());
+        }
+    }
 
-        proc_gene.set_k(RandomFloat(0f,1f));
+    // mutate the genome
+    void mutate(float rand_lvl) // rand_lvl dictates how much we mutate by. 1 is complete unrestained, 0  is none
+    {
+        // TODO
+        // go through each gene
+        // for each
+        //
+        // decide if we're replacing completely
+        // if not, tweak the scaling function
+    }
 
+    // run the genome on a data vector
+    float produce(std::vector<float> data)
+    {
+        // go down the genes and sum each result
+        float result = 0;
+
+        for(int i = 0; i < genes.size(); i++)
+        {
+            result += genes[i]->produce(data[genes[i]->get_dataIndex()]);
+        }
+
+        return result;
+    }
+
+private:
+
+    gene* newGene()  // generated a new random gene
+    {
+        gene* proc_gene = new gene;
+
+        // set sign
+        if(RandomInt(0,100) % 2 == 1)   // random yes/no
+            proc_gene->set_sign((float) -1);
+        else
+            proc_gene->set_sign((float) 1);
+
+        //set k
+        proc_gene->set_k(RandomFloat(-1.0f, 1.0f));
+
+        //set dataIndex
+        proc_gene->set_dataIndex(RandomInt(0,X));
+
+        // set a random function from functions
+        proc_gene->set_gene_func(functions[RandomInt(0,functions.size())]);
+
+        return proc_gene;
     }
 
 };
 
 
-int main() {
+int main()
+{
+    srand(time(NULL));  // seed RNG
+
+    set_func_arry();
+
+    genome mytestgenome;
+
+    std::cout << "DARWIN_TRADER - Running" << std::endl;
 
 
-
-
-
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+
 
 // fill the global list of functions THIS MUST HAPPEN BEFORE GENES ARE EVER TOUCHED
 void set_func_arry()
@@ -88,9 +170,15 @@ float RandomFloat(float a, float b)
     float random = ((float) rand()) / (float) RAND_MAX;
     float diff = b - a;
     float r = random * diff;
-    return a + r;
+    float result = a + r;
+    return result;
 }
 
+// generates random number in the range a-b
+int RandomInt(int a, int b)
+{
+    return (rand() % (b + 1 - a)) + a;
+}
 
 //-------------gene functions-------------------------------------------------------------------------------------------
 
